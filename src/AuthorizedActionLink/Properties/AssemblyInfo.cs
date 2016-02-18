bf012c6654cc0a56941d2d7e470fc81c8d058b97
Bitmapped <bpowell@bitmapped.net>
2016-02-17 21:01:10 -0500

    Set package description.

--- src/AuthorizedActionLink/Properties/AssemblyInfo.cs
+++ src/AuthorizedActionLink/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AuthorizedActionLink")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("HtmlHelper for creating ActionLinks if current user is authorized to view destination page.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Brian M. Powell")]
 [assembly: AssemblyProduct("AuthorizedActionLink")]
